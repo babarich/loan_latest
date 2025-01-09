198b83e81ffe2d937a602f45ee1fd9529686a46f
babarich <richnaldcharles1995@gmail.com>
2025-01-09 18:15:21 +0300

    changes in added

--- app/Traits/HasCompanyTrait.php
+++ app/Traits/HasCompanyTrait.php
@@ -6,12 +6,5 @@ use Illuminate\Database\Eloquent\Builder;
 
 trait HasCompanyTrait
 {
-    protected static function bootHasCompanyTrait()
-    {
-        static::addGlobalScope('company', function (Builder $builder) {
-            if (auth()->check() && auth()->user()->com_id) {
-                $builder->where('com_id', auth()->user()->com_id);
-            }
-        });
-    }
+    
 }
